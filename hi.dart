84f3c266750e5c01ee1399b5cf0e95e9d1fbb3a9
danielsumah <danielsumah@gmail.com>
2022-08-12 22:58:22 +0100

    primitivetypes in dart

--- hi.dart
+++ hi.dart
@@ -1,7 +1,19 @@
 import 'dart:io';
 
 void main() {
-  stdout.writeln('What is your name : ? ');
-  String? name = stdin.readLineSync();
-  print('My name is $name');
+  // stdout.writeln('What is your name : ? ');
+  // String? name = stdin.readLineSync();
+  // print('My name is $name');
+
+  // there are
+  int amount1 = 200;
+
+  double doubleAMount = 20.12;
+
+  String myName = 'Daniel';
+
+  bool isLogin = false;
+
+  dynamic changeableVariable = 100;
+  changeableVariable = 'smile';
 }
