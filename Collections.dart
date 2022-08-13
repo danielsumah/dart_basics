e02d4cb087352937749d9084b4553a0c94a1c414
danielsumah <danielsumah@gmail.com>
2022-08-13 10:23:05 +0100

    lists

---
+++ Collections.dart
@@ -0,0 +1,18 @@
+// Collection
+void main() {
+  //List
+  // List names = ['Jack', 'Jill'];
+  // for (var i in names) print(i);
+  // print(names.length);
+
+  // var many = ['Daniel', 10, 1.1]; //object list
+  // List<String> stringList = ['Daniel'];
+  // stringList.add('Tobi');
+  // print(stringList);
+
+  // List<String> stringList = const ['Daniel']; // a constant string list
+
+  //copy a list
+  // var albums = [1,2,3,4,5];
+  // var albumsCopy = [...albums];
+}
