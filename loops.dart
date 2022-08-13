9a48df6fa628b23d6d267226a284588b29d8901b
danielsumah <danielsumah@gmail.com>
2022-08-13 10:14:31 +0100

    loops

---
+++ loops.dart
@@ -0,0 +1,40 @@
+//  five type of loops in darts
+
+main() {
+  // 1. Standard for loop
+  // int count = 10;
+  // for (var i = 0; i < count; i++) {
+  //   print(i);
+  // }
+
+  // 2.for-in loop
+  // var numbers = [1, 2, 3, 4];
+  // for (var item in numbers) {
+  //   print(item);
+  // }
+
+  //3. for Each loop
+  // var num = [1, 2, 3];
+  // num.forEach((element) {
+  //   print(element);
+  // });
+
+  // 4. while loop
+  // var num = 5;
+  // while (num > 0) {
+  //   print('cool $num');
+  //   num -= 1;
+  // }
+
+  // using break and continue
+  // break
+  // for (var i = 0; i < 10; i++) {
+  //   if (i > 5) break;
+  //   print(i);
+  // }
+  //continue
+  // for (var i = 0; i < 10; i++) {
+  //   if (i % 2 == 0) continue;
+  //   print("Odd: $i");
+  // }
+}
