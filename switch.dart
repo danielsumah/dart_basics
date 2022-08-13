be66ae359abb5dd5cc43cf6bad466a21a3c79b3e
danielsumah <danielsumah@gmail.com>
2022-08-13 09:33:47 +0100

    switch statment

---
+++ switch.dart
@@ -0,0 +1,13 @@
+main() {
+  int num = 2;
+  switch (num) {
+    case 0:
+      print('Even');
+      break;
+    case 1:
+      print('Odd');
+      break;
+    default:
+      print('Confused');
+  }
+}
