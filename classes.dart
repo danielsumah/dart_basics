85834b9d44e0df005118fc886a70dff869f0e4a0
danielsumah <danielsumah@gmail.com>
2022-08-14 16:23:58 +0100

    more on classes

---
+++ classes.dart
@@ -0,0 +1,34 @@
+class Person {
+  String? name;
+  int? age;
+  final role = 'student'; // this is not changeable
+
+  //this is a class property..
+  //It is the same for all instance,
+  //and can be accessesed using Person.position
+  static const int position = 1;
+
+  Person(String name, [int age = 18]) {
+    this.name = name;
+    this.age = age;
+  }
+
+  // /named constructor
+  Person.guest() {
+    name = 'Guest';
+    age = 0;
+  }
+
+  void showOutput() {
+    print(name);
+    print(age);
+  }
+}
+
+void main() {
+  Person p1 = Person('Daniel', 21);
+  p1.showOutput();
+
+  Person newGuest = Person.guest();
+  newGuest.showOutput();
+}
